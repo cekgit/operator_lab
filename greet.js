f35ef126a8f16eaf34f0c4bd32b385a81fdf11ed
Your Name <you@example.com>
2026-05-02 12:09:55 -0700

    Initial commit

---
+++ greet.js
@@ -0,0 +1,7 @@
+const x = "ErdemK";
+function my_function(x) {
+    for (let y = 9; y >= 0; y--) {
+        console.log(x, y);
+    }
+}
+my_function(x);
---
+++ greet.py
@@ -0,0 +1,5 @@
+x = "Erdem"
+def my_function(x):
+    for y in range(9, -1, -1):
+        print(x, y)
+my_function(x)
