ac71690706e125c574fdf99c3e6d8c2f02550d6b
cekgit <erdem_karaca@yahoo.com>
2026-05-18 13:08:19 -0700

    Update module_six_javascript.js

--- module_six_javascript.js
+++ module_six_javascript.js
@@ -1,7 +1,7 @@
 const scores = [85, 87, 90, 94, 88];
 const avg = scores.reduce((a, b) => a + b, 0) / scores.length;
 let result;
-if (avg < 95) {
+if (avg > 95) {
     result = "Meeting Expectations";
 } else {
     result = "Needs Improvement";
